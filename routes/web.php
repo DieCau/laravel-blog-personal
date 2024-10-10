9cf3128707d0e00406cd2abaf0eb2b33cabd2189
Diego Caucota <diegocaucota@gmail.com>
2024-10-10 12:42:35 -0300

    add: controllers

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','welcome')->name('home');
-Route::view('blog','blog')->name('blog');
+Route::view('blog', 'blog')->name('blog');
 Route::view('nosotros','about')->name('about');
 Route::view('contacto','contact')->name('contact');
 
