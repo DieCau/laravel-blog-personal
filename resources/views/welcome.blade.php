a3900046cd2eb825af3672ea00eb9398b5cf8ce1
Diego Caucota <diegocaucota@gmail.com>
2024-10-10 09:35:18 -0300

    commit initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Inicio</title>
+</head>
+<body>
+<ul>
+    <li><a href="<?= route('home')?>">Home</a></li>
+    <li><a href="<?= route('blog')?>">Blog</a></li>
+    <li><a href="<?= route('about')?>">Nosotros</a></li>
+    <li><a href="<?= route('contact')?>">Contacto</a></li>
+</ul>
+<h1>Inicio</h1>
+</body>
+</html>
